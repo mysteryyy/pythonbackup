8a2e4955443d4a5e6b9af5c6352cb6d841411a09
mysteryyy <yishusahil7@gmail.com>
2020-12-07 08:00:03 +0530

    Last Sync: 2020-12-07 08:00:03

--- garchtest.py
+++ garchtest.py
@@ -23,19 +23,23 @@ from scipy.integrate import quad,dblquad
 from scipy.special import  gamma
 from arch import arch_model
 import pyflux as pf
+import sys
 import investpy
 #Main.include("try3.jl")
-pos=int(input("enter stock pos: "))
-lb = int(input("enter no of returns: "))
-k=pd.read_pickle('/home/sahil/projdir/dailydata.pkl')
-k1=k[k.Symbol==k.Symbol.unique()[pos]].iloc[-lb:]
-k1=investpy.search_quotes(text='GAYAPROJ',products=['stocks'],countries=['India'],n_results=2)[0].retrieve_historical_data(from_date='01/01/2019',to_date='17/11/2020')
+#pos=int(input("enter stock pos: "))
+#lb = int(input("enter no of returns: "))
+#k=pd.read_pickle('/home/sahil/projdir/dailydata.pkl')
+#k1=k[k.Symbol==k.Symbol.unique()[pos]].iloc[-lb:]
+#k1=investpy.search_quotes(text='GAYAPROJ',products=['stocks'],countries=['India'],n_results=2)[0].retrieve_historical_data(from_date='01/01/2019',to_date='07/12/2020')
+#k1.to_pickle("todays_stock.pkl")
+k1 = pd.read_pickle("todays_stock.pkl")
 k1['dayret'] = ((k1.Close/k1.Open)-1)*100
 #k1['dayret'] = k1.Close.pct_change()*100
 k1 = k1.dropna()
 k1 = k1.iloc[-300*3:]
 #mod =arch_model(k1.dayret,vol='GARCH',p=2,o=2,q=7,dist='StudentsT')
-model = pf.EGARCH(np.array(k1.dayret.dropna()),p=2,q=1)
-result=model.fit(method='BBVI',iterations=500,optimizer='ADAM')
-plt.plot_ppc(200)
-
+model = pf.EGARCH(np.array(k1.dayret.dropna()),p=2,q=2)
+model.add_leverage()
+result=model.fit()
+print(type(result))
+print(result)
