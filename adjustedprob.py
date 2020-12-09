9a6e37b1134737d499c46c9bf147d29196ea4568
mysteryyy <yishusahil7@gmail.com>
2020-12-09 10:00:03 +0530

    Last Sync: 2020-12-09 10:00:03

--- adjustedprob.py
+++ adjustedprob.py
@@ -17,11 +17,13 @@ def t_dist(x,nu,s):
    return ((s**-1)*gamma((nu+1)/2)/((nu**.5)*np.pi**.5*gamma(nu/2)))*(1+(x/s)**2/nu)**(-(nu+1)/2)
 
 def slprobcalc(sl,al,t1):
-    slprob= quad(lambda x:(t1**(al))*t_dist((x-u)*(t1**(al)),nu,s),-lim,sl)
+    slprob= quad(lambda x:(t1**(al))*t_dist((x-u)*(t1**(al)),nu,s),-lim,sl)[0]
     print(slprob)
     print(str(t1) + " done")
     return slprob
 
-scaledprobs = [slprobcalc(-1,.6,i) for i in range(1,300)]
-plt.plot(scaledprobs)
-plt.show()
+scaledprobs = [slprobcalc(-4,.6,i) for i in range(1,300)]
+print(dblquad(lambda x,t1:(t1**(al))*t_dist((x-u)*(t1**(al)),nu,s),-lim,-4,lambda t1:1,lambda t1:30)[1])
+
+#plt.plot(scaledprobs)
+#plt.show()
