702f91da9a3c79740f8915696031fb7db8c10725
mysteryyy <yishusahil7@gmail.com>
2020-12-09 07:00:02 +0530

    Last Sync: 2020-12-09 07:00:02

--- integrationtest.py
+++ integrationtest.py
@@ -26,26 +26,27 @@ l=0
 #    print(s)
 #    print(str(i)+" c")
 al=1.4
-nu=2.42
-s=1.88
+nu=4.52
+s=1.72
 u=0
+lim=20
 def t_dist(x,nu,s):
 
    return ((s**-1)*gamma((nu+1)/2)/((nu**.5)*np.pi**.5*gamma(nu/2)))*(1+(x/s)**2/nu)**(-(nu+1)/2)
 
 def slprobcalc(sl,al):
     
-    slprob= dblquad(lambda x,t1:(t1**al)*t_dist((x-u)*t1**al,nu,s),sl,-5,lambda t:1,lambda t:500)[0]
+    slprob= dblquad(lambda x,t1:(t1**al)*t_dist((x-u)*t1**al,nu,s),sl,-lim,lambda t:1,lambda t:500)[0]
     return slprob
 def utility(sl):
-   slprob= dblquad(lambda x,t1:(t1**al)*t_dist((x-u)*t1**al,nu,s),sl,-5,lambda t:1,lambda t:500)[0]
+   slprob= dblquad(lambda x,t1:(t1**al)*t_dist((x-u)*t1**al,nu,s),sl,-lim,lambda t:1,lambda t:500)[0]
    #slprob=0
    #for i in range(1000):
    #  al = np.random.uniform(1.2,1.8)
    #  slprob=slprob+slprobcalc(sl,al)
    print(str(sl)+" alpha done")
    #slprob = slprob*.6/1000
-   return sl*slprob+(1-slprob)*quad(lambda x:x*t_dist((x-u),nu,s),sl,5)[0]
+   return sl*slprob+(1-slprob)*quad(lambda x:x*t_dist((x-u),nu,s),sl,lim)[0]
 slrange = np.arange(-5,-0.1,0.1)
 uts = [utility(i) for i in slrange]
 plt.plot(slrange,uts)
