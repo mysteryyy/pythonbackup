e7680d4bc2b3e1397cd42451a940c695318cc133
mysteryyy <yishusahil7@gmail.com>
2020-12-16 22:30:03 +0530

    Last Sync: 2020-12-16 22:30:03

--- garchfilt.py
+++ garchfilt.py
@@ -25,17 +25,23 @@ from arch import arch_model
 import pyflux as pf
 import sys
 import investpy
-k1=investpy.search_quotes(text='TATASTEEL',products=['stocks'],countries=['India'],n_results=2)[0].retrieve_historical_data(from_date='01/01/2018',to_date='16/12/2018')
+import scipy
+k1=investpy.search_quotes(text='TATASTEEL',products=['stocks'],countries=['India'],n_results=2)[0].retrieve_historical_data(from_date='01/01/2017',to_date='16/12/2017')
 if path.exists("/home/sahil/pythonbackup/todays_stock.pkl"):
  os.remove("/home/sahil/pythonbackup/todays_stock.pkl")
 else:
  k1.to_pickle("todays_stock.pkl")
  
 k1['dayret'] = ((k1.Close/k1.Open)-1)*100
+k1['logdayret'] = np.log((k1.Close/k1.Open))*100
 #k1['dayret'] = k1.Close.pct_change()*100
 k1 = k1.dropna()
 print("long term hold ret")
 print(((k1.Close[-1]/k1.Open[0])-1)*100)
 print("intraday hold ret")
 print((((k1.Close/k1.Open)-1)*100).sum())
+print("normal ret skew")
+print(scipy.stats.skew(k1.dayret))
+print("log ret skew")
+print(scipy.stats.skew(k1.logdayret))
 
