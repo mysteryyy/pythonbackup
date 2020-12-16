4eb9a4ab2b74ce2133091fdda5a120259c354484
mysteryyy <yishusahil7@gmail.com>
2020-12-16 09:30:03 +0530

    Last Sync: 2020-12-16 09:30:03

---
+++ garchfilt.py
@@ -0,0 +1,31 @@
+import datetime
+import nolds
+from datetime import date, timedelta
+from numpy.lib.stride_tricks import as_strided
+import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt1
+from scipy.stats import norm
+import pandas as pd
+import numpy as np
+from  pathlib import Path
+from os import path
+import shutil
+import os
+import pdb
+from mle import *
+#import julia
+import scipy.optimize as opt
+from julia.api import Julia
+#j=julia.Julia(compiled_modules=False)
+#from julia import  Man
+from scipy.integrate import quad,dblquad
+#from pybayes.pdfs import CPdf
+from scipy.special import  gamma
+from arch import arch_model
+import pyflux as pf
+import sys
+import investpy
+
+#k1=investpy.search_quotes(text='AARTIIND',products=['stocks'],countries=['India'],n_results=2)[0].retrieve_historical_data(from_date='01/01/2019',to_date='07/12/2020')
+
+k1.to_pickle("todays_stock1.pkl")
