8d77f50fc5a35bf26c443afed78d7bd88cae026b
mysteryyy <yishusahil7@gmail.com>
2020-12-28 23:30:04 +0530

    Last Sync: 2020-12-28 23:30:04

--- tftryout.py
+++ tftryout.py
@@ -1,4 +1,27 @@
-import tensorflow as tf
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+
+import tensorflow.compat.v2 as tf
+import tensorflow_probability as tfp
+from functools import *
+from tensorflow_probability import bijectors as tfb
+from tensorflow_probability import distributions as tfd
+from tensorflow_probability.python.internal import dtype_util
+from tensorflow_probability.python.internal import prefer_static as ps
+from tensorflow_probability.python.internal import tensorshape_util
+tf.enable_v2_behavior()
+jds = tfd.JointDistributionSequential([
+    tfd.Normal(loc=0., scale=1.),   # m
+    tfd.Normal(loc=0., scale=1.),   # b
+    lambda b, m: tfd.Normal(loc=m*X + b, scale=1.) # Y
+])
+def _make_val_and_grad_fn(value_fn):
+  @functools.wraps(value_fn)
+  def val_and_grad(x):
+    return tfp.math.value_and_gradient(value_fn, x)
+  return val_and_grad
+print('gradient ',tfp.math.value_and_gradient(jds,[1,2,3]))
 import numpy as np
 x = tf.Variable(0.1)
 beta = tf.Variable(1.36)
