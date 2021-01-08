fa3b135c02e1bc0ddd33ec0cfb4def76b4398294
mysteryyy <yishusahil7@gmail.com>
2021-01-08 13:30:04 +0530

    Last Sync: 2021-01-08 13:30:03

--- nig.jl
+++ nig.jl
@@ -1,7 +1,8 @@
 using SpecialFunctions
 using Distributions
 using Zygote
-
+using Flux
+using PyCall
 function nigpdf1(mean,scale,alpha,beta,x)
           extra = (alpha^2-beta^2)^.5
           num = alpha*scale*besselk(1,alpha*(scale^2 + (x-mean)^2)^.5)*exp(extra*scale+beta*(x-mean))
