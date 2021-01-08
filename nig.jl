a3d3272cc18cea87ff01968caf5dd303fa5f2514
mysteryyy <yishusahil7@gmail.com>
2021-01-08 10:00:02 +0530

    Last Sync: 2021-01-08 10:00:02

--- nig.jl
+++ nig.jl
@@ -2,11 +2,10 @@ using SpecialFunctions
 using Distributions
 using Zygote
 
-function nigpdf(mean,scale,alpha,beta,x)
+function nigpdf1(mean,scale,alpha,beta,x)
           extra = (alpha^2-beta^2)^.5
           num = alpha*scale*besselk(1,alpha*(scale^2 + (x-mean)^2)^.5)*exp(extra*scale+beta*(x-mean))
           den = pi*(scale^2 + (x-mean)^2)^.5
           pdf = num/den
           return pdf
 end
-gradient(beta->nigpdf(0,2.3,11.2,beta,2),-.3)
