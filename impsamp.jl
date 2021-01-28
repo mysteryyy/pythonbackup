cf7c2795bc7dbbcafff6141bc65dc204d8295d0c
mysteryyy <yishusahil7@gmail.com>
2021-01-28 18:00:13 +0530

    Last Sync: 2021-01-28 18:00:13

--- impsamp.jl
+++ impsamp.jl
@@ -15,6 +15,16 @@ using DataFrames
 using Debugger
 using Turing,StatsBase,StatsPlots,MCMCChains
 using Parameters
+using HDF5
+include("pfilt.jl")
+f = h5open("stocksdata.hdf5","r")
+vars = read(f,"ZEEL/samples")
+w = read(f,"ZEEL/weights")
+mean_mu = read(f,"ZEEL/mean_mu")
+mean_sd = read(f,"ZEEL/mean_sd")
+w = w/sum(w)
+w,vars=gen_sample(w,vars)
+mean1 = rand(Normal(mean_mu,mean_sd),length(vars))
 function likelihood(x,vol)
 	mean_mu=pf.mean_mu
 	mean_sd=pf.mean_sd
@@ -25,4 +35,12 @@ function likelihood(x,vol)
 	end
 	return prob
 end
-
+s=0
+for i in mean1
+	for j in vars
+	 global s
+	 x=rand(Normal(i,exp(j)),1)[1]
+	 s=s+((x-i)/exp(j))^3
+	end
+end
+print(s/(length(vars)^2))
