e6c729ac05c505b6965b90d70e17403ee97811cb
mysteryyy <yishusahil7@gmail.com>
2021-03-31 17:30:02 +0530

    Last Sync: 2021-03-31 17:30:02

--- legalproj.py
+++ legalproj.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import numpy as np
+from dateutil import parser
 df = pd.read_csv('/home/sahil/Downloads/LegalTrackerDated.csv')
+df = df[df['Deal Created date'].notna()]
+df = df[df['Date of Initial Review by Legal'].notna()]
+df['Deal Created date']=df['Deal Created date'].apply(lambda x:parser.parse(x))
+df['Date of Origination']=df['Date of Origination'].apply(lambda x:parser.parse(x))
+df['Date of Initial Review by Legal']=df['Date of Initial Review by Legal'].apply(lambda x:parser.parse(x))
+df['initreview']=df['Date of Initial Review by Legal']
+df['Turnaround Time'] = (df.initreview- df['Date of Origination']).apply(lambda x:x.days)
+df['qornot']=df['Whether Q Template \n(Yes / No)']
