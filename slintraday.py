7bd962560e159f182afc581a011306df4f71846a
mysteryyy <yishusahil7@gmail.com>
2021-05-13 18:30:03 +0530

    Last Sync: 2021-05-13 18:30:03

--- slintraday.py
+++ slintraday.py
@@ -16,24 +16,40 @@ import pdb
 from mle import *
 
 pr = pd.DataFrame(columns=["open","high","low","close",],data=np.ones((20,4)))
-pr.loc[17,'low']=0.97
-pr['oh']=(pr.high-pr.open)/pr.open * 100
-pr['ol']=(pr.low-pr.open)/pr.open * 100
-pr['oc']=(pr.close-pr.open)/pr.open * 100
+pr.loc[14,'close']=0.995
+pr.loc[15,'open']=0.995
+pr.loc[15,'close']=0.990
+pr.loc[17,'low']=0.99
 print(pr)
 s=0
 sl=-1
 tp=2
 stopped_out=0
-for index,row in pr.iloc[0:len(pr)-1].iterrows():
-   
-   if (s+row.oh>tp):
-       s=tp
-       stopped_out=1
-   elif(s+row.ol<sl):
-       s=sl
-       stopped_out=1
-   else:
-       s=s+row.oc
+def simulate_trade(pr,long_short):
+    pr['oh']=(pr.high-pr.open)/pr.open * 100
+    pr['ol']=(pr.low-pr.open)/pr.open * 100
+    pr['oc']=(pr.close-pr.open)/pr.open * 100
+    s=0
+    for index,row in pr.iloc[0:len(pr)-1].iterrows():
+        if(long_short=='long'): 
+           if (s+row.oh>tp):
+               s=tp
+               stopped_out=1
+           elif(s+row.ol<sl):
+               s=sl
+               stopped_out=1
+           else:
+               s=s+row.oc
+        else:
+           if (s-row.oh>tp):
+               s=sl
+               stopped_out=1
+           elif(s+row.ol<sl):
+               s=tp
+               stopped_out=1
+           else:
+               s=s-row.oc
+
+    return s
  
 print(s)
