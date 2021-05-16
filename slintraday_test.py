f912d33d23770769cbd492319e3bacf28e9d4c99
mysteryyy <yishusahil7@gmail.com>
2021-05-16 13:00:02 +0530

    Last Sync: 2021-05-16 13:00:02

--- slintraday_test.py
+++ slintraday_test.py
@@ -2,12 +2,12 @@ import unittest
 import pandas as pd
 import numpy as np
 import slintraday
-
+from slintraday import extract_signal_trades,simulate_trade
 class TestTrades(unittest.TestCase):
     
     pr = pd.DataFrame(columns=["open","high","low","close","signal",],data=np.ones((20,5)))
 
-    def sl_shortl(self,pr):
+    def test_sl_short(self):
         pr=self.pr
         pr['signal']=0
         pr.loc[10,'signal']=-1
@@ -15,7 +15,7 @@ class TestTrades(unittest.TestCase):
         pr.loc[13,'low']=0.97
         pr.loc[15,'close']=1.012
 
-        self.assertEqual(slintraday().extract_signal_trades(pr), -1, "Should be -1")
+        self.assertEqual(extract_signal_trades(pr), -2, "Should be -1")
 
 
 
