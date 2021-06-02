2a203df51abacc1a6ead75ec4f9eefe80cd1c4ef
mysteryyy <yishusahil7@gmail.com>
2021-06-02 17:30:16 +0530

    Last Sync: 2021-06-02 17:30:16

--- tats.py
+++ tats.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import datetime
-df = pd.read_csv("/home/sahil/Downloads/AnalysisSheet.csv")
+df = pd.read_csv("/home/sahil/Downloads/AnalysisSheet4.csv")
 print(df.columns)
 df['title'] = df['Title of Document']
 df['doa'] = pd.to_datetime(df['Date of Origination'])
@@ -14,8 +14,8 @@ df['TATC'] = (df.doe-df.doi)
 df['TATC']= df['TATC']/np.timedelta64(1,'D')
 df1=df[(df.title=='NDA - client') | (df.title=='NDA - Vendor')]
 df['title'] = df.title.apply(lambda x:x[0:3] if isinstance(x,str) else "")
-df1=df[df.title=='MSA']
+df1=df[df.title=='NDA']
 #by date
-print(df1[(df1.doa>=datetime.date(2021,1,1)) & (df1.doa<=datetime.date(2021,3,15))]['TATC'].mean())
-print(df1[(df1.doa>=datetime.date(2021,3,15)) & (df1.doa<=datetime.date(2021,5,31))]['TATC'].mean())
+print(df1[(df1.doa>=datetime.date(2021,1,1)) & (df1.doa<=datetime.date(2021,3,15))]['TAT.1'].mean())
+print(df1[(df1.doa>=datetime.date(2021,3,15)) & (df1.doa<=datetime.date(2021,5,31))]['TAT.1'].mean())
 
